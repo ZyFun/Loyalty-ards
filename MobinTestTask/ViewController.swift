1265c203a6185da466143572b597757ab9c21e9d
Dmitriy Danilin <danilindn@icloud.com>
2023-04-13 08:06:54 +0300

    Initial Commit

---
+++ MobinTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobinTestTask
+//
+//  Created by Дмитрий Данилин on 13.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
